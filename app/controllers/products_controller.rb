9b091910b9442b4c062443c20ad6e75812fa8dbf
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2025-03-03 09:09:49 +0900

    Productの新規作成機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,26 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.expect(product: [:name])
   end
 end
